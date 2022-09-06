6b14765db23cc64e795cbaa64846858183a91dbd
Yayanfr20 <yayanfathurohman20@gmail.com>
2022-09-07 01:15:51 +0700

    Header Landing Page

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -12,12 +12,22 @@
 
   <!-- link favicon -->
   <link rel="shortcut icon" href="<?= BASEURL; ?>/img/SDT.png" type="images/x-icon">
+
+  <style>
+    .nav-link {
+      color: white;
+    }
+
+    .nav-link:hover {
+      color: grey;
+    }
+  </style>
 </head>
 
 <body>
 
   <!-- navbar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
     <div class="container-fluid">
       <a class="navbar-brand text-primary" href="#">SDT <span class="text-secondary">PROJECTS</span><span style="font-size: 8px;">MVC</span></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,7 +36,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">About</a>
@@ -52,5 +62,4 @@
       </div>
     </div>
   </nav>
-  <br><br>
   <!-- end navbar -->
