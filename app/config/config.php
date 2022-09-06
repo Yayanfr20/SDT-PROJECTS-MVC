b50f6066ce786781efaee6df649a1c4e91b988ce
Yayanfr20 <yayanfathurohman20@gmail.com>
2022-09-06 22:14:42 +0700

    koneksi database online

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,13 @@
 <?php
-require_once __DIR__."/../utils/Helpers.php";
+require_once __DIR__ . "/../utils/Helpers.php";
 
-class Config {
+class Config
+{
   const PUBLIC = "/public";
 }
 
 define('BASEURL', Helpers::getBASEURL(Config::PUBLIC));
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'sdt_projects_mvc');
+define('DB_HOST', 'sql6.freesqldatabase.com');
+define('DB_USER', 'sql6517581');
+define('DB_PASS', 'gBUpq1uWYV');
+define('DB_NAME', 'sql6517581');
